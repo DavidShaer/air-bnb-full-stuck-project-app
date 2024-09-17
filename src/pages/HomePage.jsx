8c3a8db91d957403bf57261110efacf634ad2905
davids890 <david.shaer89@gmail.com>
2024-09-17 22:56:15 +0300

    update first layout

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,9 +1,12 @@
-
 export function HomePage() {
-    return (
-        <section>
-            <h1>Home sweet Home</h1>
-        </section >
-    )
+  return (
+    <section className="home-page">
+      <section className="header">header</section>
+      <section className="header-search">header-search</section>
+      <section className="main-filter">main-filter</section>
+      <section className="main-A">main-A</section>
+      <section className="main-B">main-B</section>
+      <section className="footer">footer</section>
+    </section>
+  );
 }
-
