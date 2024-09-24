14e56eba175c8fbf9cd92f3442934d9e3ca01c0f
Omerf130 <ofaclieru@gmail.com>
2024-09-24 22:14:38 +0300

    slick slider added

--- src/cmps/AppFilterNav.jsx
+++ src/cmps/AppFilterNav.jsx
@@ -1,42 +1,46 @@
 import path from "path-browserify";
 import React from "react";
 import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 
 export function AppFilterNav() {
   const images = import.meta.glob("../assets/images/*.{jpg,png,svg}");
 
   const settings = {
-    dots: true,
-    infinite: true,
-    speed: 500,
-    slidesToShow: 5,
-    slidesToScroll: 5,
+    infinite: false,
+    speed: 700,
+    slidesToShow: 7,
+    slidesToScroll: 6,
+    arrows: true,
   };
 
   return (
-    // <div className="slider-container">
-    //   <Slider {...settings}>
-    <div className="filter-row-container">
-      {/* <Slider {...settings}> */}
-      {Object.keys(images).map((image_path) => {
-        const image_path_new = image_path.replace(
-          "../assets/images/",
-          "/air-bnb-full-stuck-project/src/assets/images/"
-        );
-        const imageFilename = path.basename(image_path);
+    <div className="app-filter-nav-container">
+      <Slider {...settings}>
+        {Object.keys(images).map((image_path) => {
+          const image_path_new = image_path.replace(
+            "../assets/images/",
+            "/air-bnb-full-stuck-project/src/assets/images/"
+          );
+          const imageFilename = path.basename(image_path);
 
-        return (
-          <div className="filter-nav-item" key={imageFilename}>
-            <div className="image-wrapper">
-              <img src={image_path_new} alt={imageFilename} className="image" />
-              <div className="image-overlay">
-                <p>{imageFilename.replace(".jpg", "").replace("_", " ")}</p>
+          return (
+            <div className="filter-nav-item" key={imageFilename}>
+              <div className="image-wrapper">
+                <img
+                  src={image_path_new}
+                  alt={imageFilename}
+                  className="image"
+                />
+                <div className="image-overlay">
+                  {imageFilename.replace(".jpg", "").replace("_", " ")}
+                </div>
               </div>
             </div>
-          </div>
-        );
-      })}
-      {/* </Slider> */}
+          );
+        })}
+      </Slider>
     </div>
   );
 }
