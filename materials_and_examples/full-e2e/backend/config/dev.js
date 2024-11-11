6478785e509268ac00d6711d0f93ca3bb91cbd8c
davids890 <david.shaer89@gmail.com>
2024-11-11 18:21:58 +0200

    add examples from class + exercise dir

---
+++ materials_and_examples/full-e2e/backend/config/dev.js
@@ -0,0 +1,4 @@
+export default {
+  dbURL: 'mongodb+srv://davidshaer89:D6319220@davidcluster0.d7y4w.mongodb.net/',
+  dbName : 'air-bnb-project-database'
+}
---
+++ materials_and_examples/full-e2e/backend/server.js
@@ -0,0 +1,68 @@
+import http from 'http'
+import path from 'path'
+import cors from 'cors'
+import express from 'express'
+import cookieParser from 'cookie-parser'
+
+import { authRoutes } from './api/auth/auth.routes.js'
+import { userRoutes } from './api/user/user.routes.js'
+import { reviewRoutes } from './api/review/review.routes.js'
+import { carRoutes } from './api/car/car.routes.js'
+import { setupSocketAPI } from './services/socket.service.js'
+
+import { setupAsyncLocalStorage } from './middlewares/setupAls.middleware.js'
+
+const app = express()
+const server = http.createServer(app)
+
+// Express App Config
+app.use(cookieParser())
+app.use(express.json())
+
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.resolve('public')))
+} else {
+    const corsOptions = {
+        origin: [   'http://127.0.0.1:3000',
+                    'http://localhost:3000',
+                    'http://127.0.0.1:5173',
+                    'http://localhost:5173'
+                ],
+        credentials: true
+    }
+    app.use(cors(corsOptions))
+}
+app.all('*', setupAsyncLocalStorage)
+
+app.use('/api/auth', authRoutes)
+app.use('/api/user', userRoutes)
+app.use('/api/review', reviewRoutes)
+app.use('/api/car', carRoutes)
+
+setupSocketAPI(server)
+
+// Make every unhandled server-side-route match index.html
+// so when requesting http://localhost:3030/unhandled-route... 
+// it will still serve the index.html file
+// and allow vue/react-router to take it from there
+
+app.get('/**', (req, res) => {
+    res.sendFile(path.resolve('public/index.html'))
+})
+
+import { logger } from './services/logger.service.js'
+const port = process.env.PORT || 3030
+
+server.listen(port, () => {
+    logger.info('Server is running on port: ' + port)
+})
+
+
+
+
+//TODO: david
+// 1. replace routes
+// 2. change api's to stay (instead car), implement the crudl, modify the var names, check which keys stays should send, 
+// in filter think which keys are important (for example - vendor in car )
+// 3. connect it to mongoDB (use Atlas direcly - no need the local one)
+// try to make it work until next Tuesday (integrate query atlist with the frontend)
